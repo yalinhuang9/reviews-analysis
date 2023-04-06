fb03e5157cb401e3a5d2b9f2657212bfa24400f5
Lynn <yalinhuang9@gmail.com>
2023-04-06 18:00:01 +0800

    filter length

--- reviews-analysis.py
+++ reviews-analysis.py
@@ -1,8 +1,21 @@
 data = []
+count = 0
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
-print(len(data))
-print(data[0])
-print('------------------------------------')
-print(data[1])
+        count += 1
+        if count % 1000 == 0:
+            print(len(data))
+print('檔案讀取完了, 總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+print('留言的平均長度是', sum_len/len(data))
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
